c4c4252e4d775b0b96fedd680315ef3eac2d08fe
Dmitry <dean.grimm@yandex.ru>
2023-01-10 10:07:46 +0800

    задача 19

--- HomeWork/task 19/Program.cs
+++ HomeWork/task 19/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+//  является ли оно палиндромом. Через строку решать нельзя.
+Console.WriteLine("введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n;
+int n2 = n;
+int m = 0;
+while (n > 0)
+{
+    n1 = n % 10;
+    m = m * 10 + n1;
+    n = n / 10;
+}
+if (m == n2)
+{
+    Console.WriteLine("это палиндром");
+}
+else Console.WriteLine("это не палиндром");
